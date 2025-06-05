324167786467b5f65b851241670593cd41695091
Ankan5960 <ankanmaity27@gmail.com>
2025-06-05 14:49:02 +0530

    after 60 min logout functin add

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -8,12 +8,27 @@ interface AuthState {
 
 const fetchUserDataFromLocalStorage = (): IUserLoginResponse | null => {
   const storage = new LocalStorage<IUserLoginResponse>();
+  const loginTime = localStorage.getItem("login_timestamp");
+
+  if (loginTime) {
+    const currentTime = Date.now();
+    const timeElapsed = currentTime - parseInt(loginTime, 10);
+
+    const maxSessionDuration = 60 * 60 * 1000;
+    if (timeElapsed > maxSessionDuration) {
+      storage.remove("user");
+      localStorage.removeItem("login_timestamp");
+      return null;
+    }
+  }
+
   return storage.get("user");
 };
 
 const setUserDataInLocalStorage = (user: IUserLoginResponse) => {
   const storage = new LocalStorage<IUserLoginResponse>();
   storage.set("user", user);
+  localStorage.setItem("login_timestamp", Date.now().toString());
 };
 
 export const useAuthStore = create<AuthState>(() => ({
@@ -35,4 +50,5 @@ export const clearAuthState = () => {
   useAuthStore.setState({ authUser: null });
   const storage = new LocalStorage<IUserLoginResponse>();
   storage.remove("user");
+  localStorage.removeItem("login_timestamp");
 };
